6fd6cce665afc614e13afb20b87c1ffa09bb5994
Dan Stine <sw@stinemail.com>
2019-04-27 18:07:01 +0000

    remove year from image filenames; states 6-15

--- scripts/download-images.sh
+++ scripts/download-images.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-curl -o 1999_DE_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/1999_DE_Proof.png/241px-1999_DE_Proof.png
-curl -o 1999_PA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/1999_PA_Proof.png/241px-1999_PA_Proof.png
-curl -o 1999_NJ_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/1999_NJ_Proof.png/241px-1999_NJ_Proof.png
-curl -o 1999_GA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/1999_GA_Proof.png/241px-1999_GA_Proof.png
-curl -o 1999_CT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/1999_CT_Proof.png/241px-1999_CT_Proof.png
+curl -o DE_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/1999_DE_Proof.png/241px-1999_DE_Proof.png
+curl -o PA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/1999_PA_Proof.png/241px-1999_PA_Proof.png
+curl -o NJ_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/1999_NJ_Proof.png/241px-1999_NJ_Proof.png
+curl -o GA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/1999_GA_Proof.png/241px-1999_GA_Proof.png
+curl -o CT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/1999_CT_Proof.png/241px-1999_CT_Proof.png
+
+curl -o MA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/2000_MA_Proof.png/243px-2000_MA_Proof.png
+curl -o MD_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/2000_MD_Proof.png/243px-2000_MD_Proof.png
+curl -o SC_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/2000_SC_Proof.png/243px-2000_SC_Proof.png
+curl -o NH_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/2000_NH_Proof.png/243px-2000_NH_Proof.png
+curl -o VA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/2000_VA_Proof.png/243px-2000_VA_Proof.png
+curl -o NY_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/2001_NY_Proof.png/239px-2001_NY_Proof.png
+curl -o NC_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/2001_NC_Proof.png/247px-2001_NC_Proof.png
+curl -o RI_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/2001_RI_Proof.png/243px-2001_RI_Proof.png
+curl -o VT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2001_VT_Proof.png/244px-2001_VT_Proof.png
+curl -o KY_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/2001_KY_Proof.png/243px-2001_KY_Proof.png
 
 
 # CloudFront error:
