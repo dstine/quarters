21f60ae55423e1fb5d988bc4262e56e2e775a9d2
Dan Stine <sw@stinemail.com>
2019-04-27 15:54:03 -0400

    add dynamodb table

---
+++ infrastructure/backend.tf
@@ -0,0 +1,21 @@
+resource "aws_dynamodb_table" "united_states" {
+  name           = "UnitedStates"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Id"
+  range_key      = "Name"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+
+  tags = {
+    terraform = "true"
+  }
+}
+
