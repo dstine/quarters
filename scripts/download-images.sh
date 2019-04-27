359ae8fca2ddc09e25024a4e91995e8db8dc3050
Dan Stine <sw@stinemail.com>
2019-04-27 19:04:00 +0000

    add states 16-50

--- scripts/download-images.sh
+++ scripts/download-images.sh
@@ -17,6 +17,42 @@ curl -o RI_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/2
 curl -o VT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/2001_VT_Proof.png/244px-2001_VT_Proof.png
 curl -o KY_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/2001_KY_Proof.png/243px-2001_KY_Proof.png
 
+curl -o TN_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/2002_TN_Proof.png/244px-2002_TN_Proof.png
+curl -o OH_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/2002_OH_Proof.png/239px-2002_OH_Proof.png
+curl -o LA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/2002_LA_Proof.png/239px-2002_LA_Proof.png
+curl -o IN_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/2002_IN_Proof.png/239px-2002_IN_Proof.png
+curl -o MS_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/2002_MS_Proof.png/239px-2002_MS_Proof.png
+curl -o IL_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/2003_IL_Proof.png/239px-2003_IL_Proof.png
+curl -o AL_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/2003_AL_Proof.png/239px-2003_AL_Proof.png
+curl -o ME_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/2003_ME_Proof.png/239px-2003_ME_Proof.png
+curl -o MO_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/2003_MO_Proof.png/239px-2003_MO_Proof.png
+curl -o AR_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/2003_AR_Proof.png/239px-2003_AR_Proof.png
+curl -o MI_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/2004_MI_Proof.png/239px-2004_MI_Proof.png
+curl -o FL_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/2004_FL_Proof.png/239px-2004_FL_Proof.png
+curl -o TX_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/2004_TX_Proof.png/239px-2004_TX_Proof.png
+curl -o IA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/2004_IA_Proof.png/239px-2004_IA_Proof.png
+curl -o WI_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/2004_WI_Proof.png/239px-2004_WI_Proof.png
+curl -o CA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/2005_CA_Proof.png/239px-2005_CA_Proof.png
+curl -o MN_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/2005_MN_Proof.png/239px-2005_MN_Proof.png
+curl -o OR_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/2005_OR_Proof.png/239px-2005_OR_Proof.png
+curl -o KS_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/2005_KS_Proof.png/239px-2005_KS_Proof.png
+curl -o WV_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/2005_WV_Proof.png/239px-2005_WV_Proof.png
+curl -o NV_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/2006_NV_Proof.png/239px-2006_NV_Proof.png
+curl -o NE_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/2006_NE_Proof.png/239px-2006_NE_Proof.png
+curl -o CO_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/2006_CO_Proof.png/240px-2006_CO_Proof.png
+curl -o ND_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/2006_ND_Proof.png/239px-2006_ND_Proof.png
+curl -o SD_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/2006_SD_Proof.png/239px-2006_SD_Proof.png
+curl -o MT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Montana_quarter%2C_reverse_side%2C_2007.png/240px-Montana_quarter%2C_reverse_side%2C_2007.png
+curl -o WA_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/2007_WA_Proof.png/239px-2007_WA_Proof.png
+curl -o ID_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/2007_ID_Proof_Rev.png/240px-2007_ID_Proof_Rev.png
+curl -o WY_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2007_WY_Proof_Rev.png/240px-2007_WY_Proof_Rev.png
+curl -o UT_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/2007_UT_Proof.png/240px-2007_UT_Proof.png
+curl -o OK_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/2008_OK_Proof.png/239px-2008_OK_Proof.png
+curl -o NM_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/2008_NM_Proof.png/239px-2008_NM_Proof.png
+curl -o AZ_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/2008_AZ_Proof.png/239px-2008_AZ_Proof.png
+curl -o AK_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/2008_AK_Proof.png/239px-2008_AK_Proof.png
+curl -o HI_Proof.png https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/2008_HI_Proof.png/239px-2008_HI_Proof.png
+
 
 # CloudFront error:
 # <H1>403 ERROR</H1>
