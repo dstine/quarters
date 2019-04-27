6af8155f932d5c2beabc2709629206f0a83e6a81
Dan Stine <sw@stinemail.com>
2019-04-27 17:54:27 +0000

    first five states

---
+++ scripts/deploy-site.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws s3 cp src/site s3://quarterhunt.com --recursive
---
+++ scripts/download-images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -O https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/1999_DE_Proof.png/241px-1999_DE_Proof.png
+curl -O https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/1999_PA_Proof.png/241px-1999_PA_Proof.png
+curl -O https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/1999_NJ_Proof.png/241px-1999_NJ_Proof.png
+curl -O https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/1999_GA_Proof.png/241px-1999_GA_Proof.png
+curl -O https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/1999_CT_Proof.png/241px-1999_CT_Proof.png
+
+
+# CloudFront error:
+# <H1>403 ERROR</H1>
+# <H2>The request could not be satisfied.</H2>
+# <HR noshade size="1px">
+# Request blocked.
+#curl https://www.usmint.gov/wordpress/wp-content/uploads/2016/06/1999-50-state-quarters-coin-delaware-uncirculated-reverse-150x150.jpg
